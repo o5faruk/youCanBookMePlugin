ccf7df0f1e59310f38966b7d77aa9091a204018d
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-02-02 11:20:26 +0530

    Added angular app for Widget section

---
+++ widget/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function (angular) {
+  angular.module('youCanBookMePluginWidget', ['ui.bootstrap'])
+    .controller('WidgetHomeCtrl', ['$scope', 'Buildfire', 'DataStore', 'TAG_NAMES', 'STATUS_CODE',
+      function ($scope, Buildfire, DataStore, TAG_NAMES, STATUS_CODE) {
+        var WidgetHome = this;
+
+      }]);
+})(window.angular);
