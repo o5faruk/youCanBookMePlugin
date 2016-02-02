c9f0075ba96fd8f318b4e6a3425d4adb33450e6d
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-02-02 12:44:24 +0530

    Saved data in content side and preload data

--- control/content/app.js
+++ control/content/app.js
@@ -6,5 +6,62 @@
       function ($scope, Buildfire, STATUS_CODE, TAG_NAMES, DataStore) {
         var ContentHome = this;
 
+        ContentHome.data = {
+          content: {
+            "subDomain": "",
+            "custom": ""
+          }
+        };
+
+        ContentHome.saveData = function (newObj, tag) {
+          if (typeof newObj === 'undefined') {
+            return;
+          }
+          ContentHome.success = function (result) {
+            console.info('Saved data result: ', result);
+          };
+          ContentHome.error = function (err) {
+            console.error('Error while saving data : ', err);
+          };
+          DataStore.save(newObj, tag).then(ContentHome.success, ContentHome.error);
+        };
+
+        ContentHome.validateUrl = function () {
+          if (ContentHome.subDomain)
+            ContentHome.data.content.subDomain = ContentHome.subDomain;
+          if (ContentHome.custom)
+            ContentHome.data.content.custom = ContentHome.custom;
+          ContentHome.saveData(ContentHome.data, TAG_NAMES.SCHEDULING_INFO);
+        };
+
+        /*
+         * Go pull any previously saved data
+         * */
+        var init = function () {
+          var success = function (result) {
+              console.info('init success result:', result);
+              if (Object.keys(result.data).length > 0) {
+                ContentHome.data = result.data;
+              }
+              if (ContentHome.data) {
+                if (!ContentHome.data.content)
+                  ContentHome.data.content = {};
+                if (ContentHome.data.content.subDomain)
+                  ContentHome.subDomain = ContentHome.data.content.subDomain;
+                if (ContentHome.data.content.custom)
+                  ContentHome.custom = ContentHome.data.content.custom;
+              }
+            }
+            , error = function (err) {
+              if (err && err.code !== STATUS_CODE.NOT_FOUND) {
+                console.error('Error while getting data', err);
+              }
+              else if (err && err.code === STATUS_CODE.NOT_FOUND) {
+                ContentHome.saveData(JSON.parse(angular.toJson(ContentHome.data)), TAG_NAMES.SCHEDULING_INFO);
+              }
+            };
+          DataStore.get(TAG_NAMES.SCHEDULING_INFO).then(success, error);
+        };
+        init();
       }])
 })(window.angular, window.buildfire);
