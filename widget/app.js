bd3ffd5331ebf87eaa8da88f4e4f701a144994b9
Faruk Alibasic <faruk.alibasic@gmail.com>
2020-12-21 19:51:50 +0300

    Add option to pass url from querystring

--- widget/app.js
+++ widget/app.js
@@ -11,7 +11,14 @@
          */
         WidgetHome.init = function () {
           WidgetHome.success = function (result) {
-            if (result.data && result.id) {
+            let urlParams = new URLSearchParams(window.location.search);
+            if (urlParams.has("bookingUrl")) {
+              WidgetHome.data = {
+                content: {
+                  custom: urlParams.get("bookingUrl")
+                }
+              }
+            } else if (result.data && result.id) {
               WidgetHome.data = result.data;
               if (!WidgetHome.data.content)
                 WidgetHome.data.content = {};
@@ -32,7 +39,7 @@
                 if (WidgetHome.data.content.custom && context.device.platform == "web") {
                   WidgetHome.isWebPlatform = true;
                 } else {
-                  if (WidgetHome.data.content.custom){
+                  if (WidgetHome.data.content.custom) {
                     buildfire.navigation.openWindow(WidgetHome.data.content.custom, "_blank");
                     buildfire.navigation.goBack();
                   }
